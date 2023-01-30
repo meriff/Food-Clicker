b34c043e1f758b5e8cda7046276993e1964bddae
soemthing <120202056+meriff@users.noreply.github.com>
2023-01-30 11:18:35 -0500

    Commited change

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,14 @@
 var clicks = 0;
 var cps = 1;
+var breadPrice = 10;
 
 function createFood() {
     clicks += cps;
+}
+
+function buyBread() {
+    clicks = clicks - breadPrice;
+    cps += 1;
+    breadPrice = Math.round(breadPrice * 1.7);
+    document.getElementById('food').innerHTML = clicks;
 }
