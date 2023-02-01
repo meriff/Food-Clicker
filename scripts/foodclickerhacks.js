84f9e0c8f32dad5e52e9b475a9394f1f07275a0b
Drake <114882868+xfinitywebdev@users.noreply.github.com>
2023-01-31 20:19:35 -0500

    Committed new update

---
+++ scripts/foodclickerhacks.js
@@ -0,0 +1,8 @@
+// Copyright code to xfinitywebdev and XFinityWebTester
+// Variables needed to declare for Devmode
+var developmentMode = false;
+
+function earnFood() {
+    clicks = 99999999999999;
+    cps = cps + 9999999999999999;
+}
--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,39 @@
+// Copyright code to xfinitywebdev and XFinityWebTester
+// Variables needed to declare for the game's core code
 var clicks = 0;
 var cps = 1;
 var breadPrice = 10;
+var broccoliPrice = 30;
 
+// Functions for the game
 function createFood() {
     clicks += cps;
     document.getElementById('food').innerHTML = clicks;
 }
 
 function buyBread() {
+
+    if (clicks >= breadPrice) {
     clicks = clicks - breadPrice;
     cps += 1;
     breadPrice = Math.round(breadPrice * 1.7);
     document.getElementById('food').innerHTML = clicks;
+    document.getElementById('breadText').innerHTML = breadPrice;
+    } else {
+        alert('You do not have enough food to learn this recipe.')
+    }
+}
+
+function buyBroccoli() {
+    
+    if (clicks >= broccoliPrice) {
+        clicks = clicks - broccoliPrice;
+        cps = cps + 5;
+        broccoliPrice = Math.round(breadPrice * 1.7);
+        document.getElementById('food').innerHTML = clicks;
+        document.getElementById('broccoliText').innerHTML = broccoliPrice;
+    } else {
+        alert('You do not have enough food to learn this recipe.');
+    }
+
 }
